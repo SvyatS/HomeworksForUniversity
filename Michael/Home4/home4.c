597d8209984eb0690923ac84281c4dc512aa51f2
SvyatS <52736791+SvyatS@users.noreply.github.com>
2020-04-22 00:16:21 +0700

    fix

--- Michael/Home4/home4.c
+++ Michael/Home4/home4.c
@@ -4,19 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int n, size, i, one;
+	int n, i, one;
 	printf("Enter power sets: ");
 	scanf("%d", &n);
 	
 	printf("Enter number of units: ");
 	scanf("%d", &one);
 	
-	printf("Enter size of array: ");
-	scanf("%d", &size);
-	int A[size];
+	int A[n];
 	
 	printf("Enter elements of array:\n");
-	for(i=0; i<size; i++){
+	for(i=0; i<n; i++){
 		scanf("%d", &A[i]);
 	}
 	printf("\n\n\n");
@@ -37,7 +35,7 @@ int main(int argc, char *argv[]) {
 		for(j=0; j<n; j++){
 			if(M[j] == 1) schet++;
 		}
-		for(j=0; j<size; j++){
+		for(j=0; j<n; j++){
 			if(i == A[j] && schet == one){
 				printf("%d   ", A[j]);
 				for(boolM = n-1; boolM >= 0; boolM--) printf("%d", M[boolM]);
